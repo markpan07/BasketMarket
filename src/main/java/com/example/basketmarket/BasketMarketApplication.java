9250bb497176abe094de0c691d93fe30a0960b05
МАРК <markpan07@mail.ru>
2023-05-08 13:09:20 +0300

    Initial commit

---
+++ src/main/java/com/example/basketmarket/BasketMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.basketmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketMarketApplication.class, args);
+    }
+
+}
